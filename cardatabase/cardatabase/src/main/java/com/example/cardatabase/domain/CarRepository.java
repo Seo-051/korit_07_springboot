22b670e88ba98ac933d384e1495bbb4bf3f2f719
Seo-051 <kingdom5437@naver.com>
2025-09-23 17:15:17 +0900

    feat 20250923

--- cardatabase/cardatabase/src/main/java/com/example/cardatabase/domain/CarRepository.java
+++ cardatabase/cardatabase/src/main/java/com/example/cardatabase/domain/CarRepository.java
@@ -1,18 +1,16 @@
-package com.example.cardatabase.domin;
+package com.example.cardatabase.domain;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.List;
 
-public interface CarRepository extends CrudRepository<Car, Long> {
-    // 브랜드로 자동차를 검색
-    List<Car> findByBrand(String brand);
+@RepositoryRestResource
+public interface CarRepository extends JpaRepository<Car, Long> {
+    // 브랜드로 자동차 검색하는 쿼리 메서드
+    List<Car> findByBrand(@Param("brand") String brand);
 
-    // 브랜드와 모델을 합쳐서 자동차를 검색
-    List<Car> findByBrandAndModel(String brand, String model);
-
-    // 브랜드
-
-    List<Car> findByColor(String white);
-    List<Car> findByModelYear(int modelYear);
+    // 색상으로 자동차 검색하는 쿼리 메서드
+    List<Car> findByColor(@Param("color") String color);
 }
