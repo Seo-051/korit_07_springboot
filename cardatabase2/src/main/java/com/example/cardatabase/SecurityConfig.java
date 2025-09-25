140bec0217ebb21d748afa246e56acd7602bc72f
user <user@userui-MacBookAir.local>
2025-09-25 17:46:45 +0900

    feat 20250925 backend protection / jwt / authentication

--- cardatabase2/src/main/java/com/example/cardatabase/SecurityConfig.java
+++ cardatabase2/src/main/java/com/example/cardatabase/SecurityConfig.java
@@ -1,10 +1,12 @@
 package com.example.cardatabase;
 
 import com.example.cardatabase.service.UserDetailsServiceImpl;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.AuthorizeHttpRequestsDsl;
@@ -21,18 +23,24 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+@Slf4j
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-    public  final UserDetailsServiceImpl userDetailsService;
+    private  final UserDetailsServiceImpl userDetailsService;
+    private final AuthenticationFilter authenticationFilter;
+    private final AuthEntryPoint exceptionHandler;
 
-    public SecurityConfig(UserDetailsServiceImpl userDetailsService){
+    public SecurityConfig(UserDetailsServiceImpl userDetailsService, AuthenticationFilter authenticationFilter, AuthEntryPoint authEntryPoint){
         this.userDetailsService = userDetailsService;
+        this.authenticationFilter = authenticationFilter;
+        this.exceptionHandler = authEntryPoint;
     }
 
     public void configGlobal (AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService);
+        auth.userDetailsService(userDetailsService).passwordEncoder(new BCryptPasswordEncoder());
     }
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -50,11 +58,15 @@ public class SecurityConfig {
                 .sessionManagement(sessionManagement ->
                         sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorizeHttpRequests ->
-                        authorizeHttpRequests.requestMatchers(HttpMethod.POST, "/login").permitAll().anyRequest().authenticated());
+                        authorizeHttpRequests.requestMatchers(HttpMethod.POST, "/login").permitAll().anyRequest().authenticated())
+                .addFilterBefore(authenticationFilter, UsernamePasswordAuthenticationFilter.class)
+        .exceptionHandling(exceptionHandling ->
+                exceptionHandling.authenticationEntryPoint(exceptionHandler));
         return http.build();
     }
-
 }
+
+
 
 
 
